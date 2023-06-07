ad064a232f6067fd18369b89d1cea92d23b0b2af
Heisenberg-x1 <chemerilcollins@gmail.com>
2023-06-07 21:00:12 +0200

    Finished prop. Made the online laptop shop. Well a part of it

--- Jot.md
+++ Jot.md
@@ -9,10 +9,8 @@ The es7 extension has some useful snippets
 
 - We can also create elements by calling the react.createElement function as follows
 
-
-
-
 <!-- *CreateElement -->
+
 ` return React.createElement('h1', {}, 'Hello React community');`
 
 - We should always return one element
@@ -31,19 +29,15 @@ The es7 extension has some useful snippets
 
     </React.Fragment>;`
 
-
-
-
 <!-- *Naming convention and jsx rules -->
+
 - camel case
 - class in html is className
 - for in html is htmlFor
 - Always have the opening tag in the same line as the return statement otherwise it wont work. You can use parethesis tho
 
-
-
-
 <!-- *Nesting the components-->
+
 - The components can be nested easily as follows
 
 ```javascript
@@ -77,26 +71,19 @@ const Image = () => (
 const Author = () => <p>Author component</p>;
 ```
 
-
-
-
 <!-- *Adding CSS -->
+
 - We create a css file inside the src folder. We can also create all th css files inside one folder, just need to change the path
 - Add a class that needs styling, well a className is added. Then style the element normally inside the css file
 
-
-
-
 <!-- *Images -->
+
 - We can use both external images and local images. External images, we need only the address to the image
 - For local image, we can create a folder for the images inside the public dir . To access the image, we just use the path foldername/image. No need to add the public folder name etc . REASON: Since the asset we are trying to access, we do not need the full address
 
 ```htm
 <img src="./images/img1.png" alt="img1" />
 ```
-
-
-
 
 <!-- *Adding internal css in the jsx -->
 
@@ -126,7 +113,24 @@ let header = 'Tittle component'
  <h1 style={{ color: "tomato", fontSize: "1.1rem" }}>{header}</h1>;
 ```
 
-
-
-
 <!-- *Props -->
+
+props(properties) is a way of passing data from the parent component to the children component. It facilitates the re usage of a component by passing different data
+
+```javascript
+function All(props) {
+  return (
+    <div className="all">
+      <h1 style={{ color: "tomato", fontSize: "1.1rem" }}>{props.title}</h1>
+      <img src={props.img} alt="img1" />
+      <p style={style1}>{props.description}</p>
+    </div>
+  );
+}
+// We can now call the component in the main component as follows with the props passed
+<All
+  title="Book1"
+  img="./images/book1.png"
+  description="Random description of the book."
+/>;
+```
--- src/index.js
+++ src/index.js
@@ -2,32 +2,118 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
+let laptop1 = {
+  name: "Mate book 14s",
+  image: "https://i.ebayimg.com/images/g/t5EAAOSwPkljCJ-k/s-l500.jpg",
+  price: "$1200",
+  description:
+    "HUAWEI MateBook 14S 2022 12th Gen Core Laptop 14.2 inch 2.5K Touch Screen 90Hz",
+};
+let laptop2 = {
+  name: "HP Envy x360",
+  image: "https://i.ebayimg.com/images/g/t5EAAOSwPkljCJ-k/s-l500.jpg",
+  price: "$800",
+  description:
+    "NEW HP Envy x360 15.6' FHD Intel Core i5 256GB SSD 8GB RAM 2-in-1 Design Win 10",
+};
+let laptop3 = {
+  name: "Huawei MateBook X Pro",
+  image: "https://i.ebayimg.com/images/g/4-oAAOSwwQdkb4jd/s-l300.jpg",
+  price: "$1900",
+  description:
+    "HUAWEI MateBook X Pro 2023 13th Gen Core Laptop 14.2 inch 3.1K Touch Screen",
+};
 const Book = () => {
   return (
-    <div className="book">
-      <All />
-      <All />
-      <All />
+    <div className="laptop-Container">
+      <Laptop
+        name={laptop1.name}
+        img={laptop1.image}
+        price={laptop1.price}
+        description={laptop1.description}
+      />
+      <Laptop
+        name={laptop2.name}
+        img={laptop2.image}
+        price={laptop2.price}
+        description={laptop2.description}
+      />
+      <Laptop
+        name={laptop2.name}
+        img={laptop2.image}
+        price={laptop2.price}
+        description={laptop2.description}
+      />
+      <Laptop
+        name={laptop3.name}
+        img={laptop3.image}
+        price={laptop3.price}
+        description={laptop3.description}
+      />
+      <Laptop
+        name={laptop2.name}
+        img={laptop2.image}
+        price={laptop2.price}
+        description={laptop2.description}
+      />
+      <Laptop
+        name={laptop3.name}
+        img={laptop3.image}
+        price={laptop3.price}
+        description={laptop3.description}
+      />
+      <Laptop
+        name={laptop2.name}
+        img={laptop2.image}
+        price={laptop2.price}
+        description={laptop2.description}
+      />
+      <Laptop
+        name={laptop3.name}
+        img={laptop3.image}
+        price={laptop3.price}
+        description={laptop3.description}
+      />
+      <Laptop
+        name={laptop2.name}
+        img={laptop2.image}
+        price={laptop2.price}
+        description={laptop2.description}
+      />
+      <Laptop
+        name={laptop3.name}
+        img={laptop3.image}
+        price={laptop3.price}
+        description={laptop3.description}
+      />
     </div>
   );
 };
-let header = 'Tittle component'
 const style1 = {
   color: "green",
-  fontSize: "0.8rem",
+  fontSize: "01rem",
+  textAlign: "center",
+  paddingTop: "10px",
 };
-function All() {
+function Laptop(props) {
   return (
-    <div className="all">
-      <h1 style={{ color: "tomato", fontSize: "1.1rem" }}>{header}</h1>;
-      <img src="./images/img1.png" alt="img1" />      
-      <p style={style1}>Author component</p>;
+    <div className="laptop">
+      <h3>{props.name}</h3>
+      <div className="laptop-img">
+        <img src={props.img} alt="" />
+      </div>
+      <section className="laptop-description">
+        <p>{props.description}</p>
+        <p style={style1}>price : {props.price}</p>
+      </section>
+  <div className="buy-now"alert-dismissible>
+        <button>Buy Now</button>
+      </div>
     </div>
   );
 }
 
 export default Book;
-
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Book />);
